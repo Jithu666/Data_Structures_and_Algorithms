8f146169a8144de7707ff49b071c70f343dd0793
Jithu666 <githendragowdakr@gmail.com>
2024-12-08 20:40:21 +0530

    Java File Operations

---
+++ File IO/src/reader.java
@@ -0,0 +1,47 @@
+import java.io.*;
+
+public class reader
+{
+    public static void main(String[] args) {
+
+        // writing to a file
+
+//        try (FileWriter writer = new FileWriter("file.txt")) {
+//            writer.write("Hello");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+        // reading from a file
+
+//        try (FileReader reader = new FileReader("Core Java/File IO/src/file.txt")){
+//            int character;
+//            while((character = reader.read()) != -1) {
+//                System.out.print((char) character);
+//            }
+//        } catch (IOException e){
+//            e.printStackTrace();
+//        }
+
+
+        // Buffered Reader
+//        try (BufferedWriter writer = new BufferedWriter(new FileWriter("file1.txt"))){
+//            writer.write("Hello Jithu ..");
+//        } catch (IOException e){
+//            e.printStackTrace();
+//        }
+
+        // Buffered Reader
+//        try(BufferedReader reader = new BufferedReader(new FileReader("Core Java/File IO/src/file1.txt"))) {
+//            String line;
+//            while ((line = reader.readLine()) != null){
+//                System.out.println(line);
+//            }
+//        } catch (IOException e){
+//            e.printStackTrace();
+//        }
+
+
+
+    }
+}
